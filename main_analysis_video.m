e92c0913720ab233c02628f4f0e01474fb6f4639
Alexander Madrigal <lxndrmdrgl@gmail.com>
2026-01-29 04:44:01 -0800

    comment matlab

--- main_analysis_video.m
+++ main_analysis_video.m
@@ -13,21 +13,21 @@ NumFrames=vidObj.NumFrames;
 i=1;
 while hasFrame(vidObj)
     frame = readFrame(vidObj);
-
-    img=im2gray(frame);
-    [n1,n2]=size(img);
+    
+    img=im2gray(frame); % grayscale the image
+    [n1,n2]=size(img); % Image size (rows, cols)
 %    imgc=adapthisteq(img);
     imgc=img;
-    imgb=imbinarize(imgc);
-    imgb = (imgb == 0); % flip it
-    cc4 = bwconncomp(imgb,4);
+    imgb=imbinarize(imgc); % automatically make black or white
+    imgb = (imgb == 0); % invert black and white
+    cc4 = bwconncomp(imgb,4); %4-connectivity
 
     % threshold to find the worm in the image
     maxpixel=2000; % max number of pixels for the worm
     minpixel=100; % min number of pixels for the worm
-    newc={};
+    newc={}; % stores only good size blobs
     for j=1:cc4.NumObjects
-        s(j)=size(cc4.PixelIdxList{1,j},1);
+        s(j)=size(cc4.PixelIdxList{1,j},1); % size of blob j
         if s(j)>minpixel && s(j)<maxpixel 
             if (isempty(newc))
                 newc{1}=cc4.PixelIdxList{1,j};
@@ -38,15 +38,19 @@ while hasFrame(vidObj)
     end
     newcsize=size(newc,2);
 
-    objsize(i)=size(newc{1},1);
+    % if no blobs pass size filter, error
 
-    vpixels=zeros(objsize(i),2);
+    objsize(i)=size(newc{1},1); % chooses the worm blob
+
+    vpixels=zeros(objsize(i),2); % converts linear indicies into (row, col) pixel coords
     for p=1:objsize(i)
         x=newc{1}(p);
+        % 1D index into 2D coordinates
         vpixels(p,1)=floor(x/n1);
         vpixels(p,2)=mod(x,n1);
     end
-
+    
+    % Find bounding box to crop image
     imin=min(vpixels(:,1));
     imax=max(vpixels(:,1));
     jmin=min(vpixels(:,2));
@@ -55,46 +59,52 @@ while hasFrame(vidObj)
     n1=imax-imin+1;
     n2=jmax-jmin+1;
 
-    ROI=zeros(n1,n2);
+    ROI=zeros(n1,n2); % Region of Interest: cropped image around worm
     for p=1:objsize(i)
         ROI(vpixels(p,1)-imin+1,vpixels(p,2)-jmin+1)=1;
     end
 
+    % Figure 1: raw cropped worm mask
     figure(1)
     subplot(1,4,1);
     imagesc(ROI);
     
+    % stats gets shape properties of the blob
     stats = regionprops(logical(ROI), 'Centroid','MajorAxisLength','MinorAxisLength','Orientation');
-
+    
+    % rotate ROI is worm is straight according to orientation
     ROIrot = imrotate(ROI,-stats.Orientation);
     subplot(1,4,2);
     imagesc(ROIrot);
-
+    
+    % Measure bounding box after rotation
     statsrot = regionprops(logical(ROIrot), 'BoundingBox');
 
-    size1=statsrot.BoundingBox(3);
-    size2=statsrot.BoundingBox(4);
-    aspectratio=max(size1,size2)/min(size1,size2);
+    size1=statsrot.BoundingBox(3); % width
+    size2=statsrot.BoundingBox(4); % height
+    aspectratio=max(size1,size2)/min(size1,size2); 
     
+    % Skeletionize to 1-pixel wide spine
     out = bwskel(logical(ROI));
 
     subplot(1,4,3);
     imagesc(out);
-    out=out(:);
-    idx1=find(out==1);
-    nematode_length=size(idx1,1);
+
+    out=out(:); % flatten into 1D
+    idx1=find(out==1); % indices of skeleton pixels
+    nematode_length=size(idx1,1); % length in pixels
  
     cc4.NumObjects=newcsize;
     cc4.PixelIdxList=newc;
 
-    L4 = labelmatrix(cc4);
+    L4 = labelmatrix(cc4); % label image: each blob has integer label
 
-    RGB_label = label2rgb(L4);
+    RGB_label = label2rgb(L4); % convert labels into colored image
 
     subplot(1,4,4);
     imshow(RGB_label);
-
-    i=i+1;
+    
+    i=i+1; % go to next frame
 end
 
 fprintf('That s all folks\n');
